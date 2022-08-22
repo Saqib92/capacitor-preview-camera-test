b63d48d4fcc1b08419de700d5ac134cdd51ca881
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-08-22 14:43:08 +0500

    plugin setup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CameraPreview, CameraPreviewOptions } from '@capacitor-community/camera-preview';
+import '@capacitor-community/camera-preview';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private alertController: AlertController
+  ) {}
+
+  ionViewDidEnter(){
+    this.startCamera();
+  }
+
+  async ionViewWillLeave(){
+    await CameraPreview.stop()
+  }
+
+  startCamera() {
+    const cameraPreviewOptions: CameraPreviewOptions = {
+      position: 'rear',
+      width: window.screen.width,
+      height: 300,
+      parent: "content",
+      toBack: true,
+      y: 56,
+      enableHighResolution: true
+    };
+    CameraPreview.start(cameraPreviewOptions)
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('canclled')
+          },
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            console.log('confirmed')
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 
 }
